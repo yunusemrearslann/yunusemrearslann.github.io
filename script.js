9fb5c275ba42e49c105ad40ef2ca8cbffba31ae1
yunusemrearslann <33065920+yunusemrearslann@users.noreply.github.com>
2020-08-24 16:41:49 +0300

    gerekli duzenlemeler yapildi

--- script.js
+++ script.js
@@ -206,3 +206,22 @@ function fauxClick(x, y) {
   fauxClick.pageY = y;
   document.dispatchEvent(fauxClick);
 }
+
+const elements = Array.from(document.querySelectorAll('.fill'));
+
+function fill(item, index) {
+  
+  const bgColor = getComputedStyle(item).backgroundColor,
+  fillLayer = document.createElement('div');
+  fillLayer.classList.add('fill-layer');
+  item.style.backgroundColor = 'transparent';
+  item.style.position = 'relative';
+  item.style.overflow = 'hidden';
+  setTimeout(function() {
+    fillLayer.style.backgroundColor = bgColor;  
+    item.appendChild(fillLayer);
+  }, index * 1000);
+  
+}
+
+elements.forEach(fill);
